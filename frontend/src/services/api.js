c3f03cd2d9bd88dea60ce0f044e00f0979710289
Ashish Kumar <ashishk792003@gmail.com>
2025-12-08 20:40:30 +0530

    changes so that it could run from localhost also

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = `${import.meta.env.baseURL}/api/transactions`;
+const API_BASE_URL = `${import.meta.env.baseURL || 'http://localhost:3000'}/api/transactions`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
