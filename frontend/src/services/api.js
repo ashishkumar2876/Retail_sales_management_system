793e0c5a6d8557f33403df1c3c478686cb66fd20
Ashish Kumar <ashishk792003@gmail.com>
2025-12-08 20:38:09 +0530

    providing backend url to frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api/transactions';
+const API_BASE_URL = `${import.meta.env.baseURL}/api/transactions`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
